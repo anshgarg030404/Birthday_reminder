67610d1089f6cf194a8c08135c5c8830e305a67a
Ansh Garg <anshgarg030404@gmail.com>
2023-08-26 19:23:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import data from "./data";
+import react,{useEffect, useState} from 'react';
 import './App.css';
 
+
+
+
+  
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  
+  const [items,setItems]=useState([]);
+  
+  useEffect(() => {
+    
+    setItems(data);
+
+  },[])
+  
+  
+  const hello=(s_No)=>{
+     const newData = items.filter((item) => item.s_No !== s_No);
+     setItems(newData)
+    //  console.log(newData);
+
+  }
+
+
+  
+   return (
+      <>
+      {items.map((person) => {
+        return (
+          <>
+            <div class='details'>
+              <h2>{person.s_No}</h2>
+              <h2>{person.name}</h2>
+              <h2>{person.birthday}</h2>
+              </div>
+              <button onClick={() => {hello(person.s_No)}}>Remove</button>
+            
+     </>
+        );
+      })}
+    </>)
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    s_No: 1,
+    name: "Ansh Garg",
+    birthday: "3rd April",
+  },
+  {
+    s_No: 2,
+    name: "Unnati Jain",
+    birthday: "8th November",
+  },
+  {
+    s_No: 3,
+    name: "Angel Garg",
+    birthday: "30th April",
+  },
+  {
+    s_No: 4,
+    name: "Vikram roy",
+    birthday: "23rd August",
+  },
+  {
+    s_No: 5,
+    name: "Sachin Yadav",
+    birthday: "13th April",
+  },
+];
+
+
+
+
+  
